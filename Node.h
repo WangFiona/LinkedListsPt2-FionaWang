6aeee0a0c0605fbe0219e30090480bb63dec701a
WangFiona <fionaw5th@gmail.com>
2022-01-13 13:51:27 -0800

    correct

--- Node.h
+++ Node.h
@@ -1,29 +1,27 @@
-#include "Node.h"
+//Header Guard
+#ifndef NODE_H
+#define NODE_H
+#include <iostream>
 #include "Student.h"
-//Constructor
-Node::Node(Student* newStudent){
-//Setting the node's student to the inputted student
-NodeStudent = newStudent;
-//Setting the next node to null
-next = NULL;
-}
+using namespace std;
 
-Node::~Node(){
-cout << "You're Deleting ... " << endl;
-//Deleting the node's student
-delete NodeStudent;
-//Setting the next node to null
-next = NULL;
-}
-//Setting the next node
-void Node::setNext(Node* newNext){
-    next = newNext;
-}
-//Returning the next node
-Node* Node::getNext(){
-    return next;
-}
-//Returning the node's student
-Student* Node::getStudent(){
-return NodeStudent;
-}
+class Node{
+  //Student to be set by constructor
+  Student* NodeStudent;    
+  //Next Node
+  Node* next;
+  
+ public:
+  //Constructor
+  Node(Student* newStudent);  
+  //Destructor
+  ~Node();
+  //Setting the next node
+  void setNext(Node* newNext);
+  //Getting the next node
+  Node* getNext();
+  //Getting the student from the node
+  Student* getStudent();
+};
+
+#endif
